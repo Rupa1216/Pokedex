c1bf60e113e0cfa8609dcef3b0fbea6928150859
Srirupa <srirupadatta@pursuit.org>
2019-02-12 21:50:59 -0500

    imported reactstrap Input component and created Header as a functional component

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import './header.css';
+import { Input } from 'reactstrap';
 
+const Header = (props) => {
+    return (
+        <div>
+
+            {/* <InputGroup> */}
+            {/* <InputGroupAddon addonType="prepend">@</InputGroupAddon> */}
+            <Input placeholder="Search..." />
+            {/* </InputGroup> */}
+            <br />
+        </div>
+    )
+}
 
 export default Header;

5e7a17852552fd990493af91a951811c0df53888
Srirupa <srirupadatta@pursuit.org>
2019-02-14 22:57:47 -0500

    added title property to state and Header component in the render

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+// import { Input } from 'reactstrap';
+import Header from './components/header';
 
 class App extends Component {
 constructor(props) {
   super(props)
 
   this.state = {
-    homepage: true,
-    pokeList: [], // have it on a JSON on a separate pg and import it to make it more readable
-    pokeProfile: {}, 
-    movePopup: false // make this an empty str. eventHandler will fill in the str and pass it into a func somewhere that makes it pop up
+    title: 'Pursuit Pokedex',
+    homepage: true, // toggle for whether user is on Homepage or Profile page
+    pokeList: [], // An arr of objs that will load in batches of 20
+    // TA suggestion: store pokelist in a JSON on a separate pg and import it to make it more readable
+    pokeProfile: {}, // Contains the response data on selected pokemon
+    moves: [], // a unique list of moves of the selected pokemon
+    show: false // toggle for whether the Move modal will render on the UI
+    // TA suggestion: store show's value in an empty str; 
+    // eventHandler will fill in the str and pass it into a func somewhere that makes modal pop up
   }
 }
 
   render() {
     return (
-      <h1>hello world</h1>
+      
+      <Header />
+      
     );
   }
 }

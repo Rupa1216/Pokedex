ac41c5172c1b46ecfc99c9188723d3d98c5bdf4a
Srirupa <srirupadatta@pursuit.org>
2019-02-14 22:53:12 -0500

    defined a functional component and returned a Jumbotron and Input field

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Jumbotron, Input } from 'reactstrap';
 import './header.css';
-import { Input } from 'reactstrap';
+
 
 const Header = (props) => {
     return (
-        <div>
-
-            {/* <InputGroup> */}
-            {/* <InputGroupAddon addonType="prepend">@</InputGroupAddon> */}
-            <Input placeholder="Search..." />
-            {/* </InputGroup> */}
+        <div className='container'>
+            <div className='row red'>
+            </div>
+            <Jumbotron className='jumbotron'>
+                <h5 className="display-5 header">Pursuit Pokedex</h5>
+            </Jumbotron>
+            <Input placeholder="Search..." className="input" />
             <br />
+            <hr></hr>
         </div>
     )
 }

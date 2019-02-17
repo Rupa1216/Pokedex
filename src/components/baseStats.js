ead9245028bf38bca44a30aff83eada4a8ee7563
Erika Perez <erikaperezvalle@pursuit.org>
2019-02-16 23:32:17 -0500

    implemented mapping the stats, css is not working properly

--- src/components/baseStats.js
+++ src/components/baseStats.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import { Table } from 'reactstrap';
 import './baseStats.css'
 
 const BaseStats = props => {
     console.log(props.profileData)//an array of stats objects
-    const stats = props.profileData[0].base_stat;
+    const stats = props.profileData;
 
-    return <div>
-        <p>{stats}</p>
+    return <>
+        <div>
+            <h2>Base Stats</h2>
+            <Table borderless>
+                {stats.map((stat, index) => {
+                    const base_stat = stat.base_stat
+                    const stat_name = stat.stat.name
+                    return <>
 
-    </div>
+                        <thead>
+                            <tr>
+                                <tb>{stat_name}</tb>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr><th>{base_stat}</th></tr>
+
+                        </tbody>
+
+
+
+
+                    </>
+                })}
+
+            </Table>
+        </div>
+    </>
+
 }
 
 

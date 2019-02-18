03e930a86f0b04e26845ce633d9c78f4a623e287
Srirupa <srirupadatta@pursuit.org>
2019-02-18 14:44:44 -0500

    imported pokeball image and inserted it into Header jsx

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Jumbotron, Input } from 'reactstrap';
 import './header.css';
-
+import pokeball from '../pokeball.png';
 
 const Header = (props) => {
     return (
         <div className='container'>
             <div className='row red'>
             </div>
+            <img src={pokeball} alt="sprite" className="imgLeft"></img>
             <Jumbotron className='jumbotron'>
-                <h5 className="display-5 header">Pursuit Pokedex</h5>
+                <h4 className="display-5 header">Pursuit Pokedex</h4>
             </Jumbotron>
             <Input placeholder="Search..." className="input" />
+            <img src={pokeball} alt="sprite" className="imgRight"></img>
             <br />
             <hr></hr>
         </div>

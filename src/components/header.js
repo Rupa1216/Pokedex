e6fdf23ff790f27734741b39519b2a726f0bff62
Srirupa <srirupadatta@pursuit.org>
2019-02-19 14:41:33 -0500

    fixed rendering of title using props passed from App.js and removed unused classNames

--- src/components/header.js
+++ src/components/header.js
@@ -4,13 +4,14 @@ import './header.css';
 import pokeball from '../pokeball.png';
 
 const Header = (props) => {
+    const title = props.data;
     return (
         <div className='container'>
-            <div className='row red'>
+            <div className='row'>
             </div>
             <img src={pokeball} alt="sprite" className="imgLeft"></img>
             <Jumbotron className='jumbotron'>
-                <h4 className="display-5 header">{this.state.title}</h4>
+                <h4 className="display-5 header">{title}</h4>
             </Jumbotron>
             <Input placeholder="Search..." className="input" />
             <img src={pokeball} alt="sprite" className="imgRight"></img>
@@ -20,4 +21,4 @@ const Header = (props) => {
     )
 }
 
-export default Header;
+export { Header } ;
